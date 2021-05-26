df38a804f60c0eb72f8f418045915128930b578e
achandra22 <achandra220@gmail.com>
2021-05-26 13:46:21 +1000

    implemented client credentials authorisation flow

--- authorisation.py
+++ authorisation.py
@@ -8,15 +8,20 @@ class SpotifyAuthorisation:
         self.client_id = client_id
         self.client_secret = client_secret
         self.access_token = None
-        self.token_expires = datetime.datetime.now()
+        self.token_expiry = datetime.datetime.now()
         self.did_expire = True
-
 
     def authorise(self):
         query = 'https://accounts.spotify.com/api/token'
         request_body = {
             "grant_type": "client_credentials"
         }
+
+        '''
+        Header Parameter (from Spotify API docs)
+        Base 64 encoded string that contains the client ID and client secret key. 
+        The field must have the format: Authorization: Basic <base64 encoded client_id:client_secret>
+        '''
         client_creds = f"{self.client_id}:{self.client_secret}"
         client_creds_b64 = base64.b64encode(client_creds.encode())
         response = requests.post(
@@ -24,43 +29,32 @@ class SpotifyAuthorisation:
             data=request_body,
             headers={"Authorization": f"Basic {client_creds_b64.decode()}"}
         )
-        # print(response.status_code)
-        # print(response.json())
 
-        if response.status_code in range(200, 299): #check whether request was valid
+        #check whether request was valid
+        if response.status_code in range(200, 299): 
             token_response_data = response.json()
             now = datetime.datetime.now()
             self.access_token = token_response_data['access_token']
-            self.expires_in = token_response_data['expires_in'] # seconds
-            expires = now + datetime.timedelta(seconds=self.expires_in)
-            self.did_expire = expires < now
+            expires_in = token_response_data['expires_in'] # seconds
+            expiry = now + datetime.timedelta(seconds=expires_in)
+            self.token_expiry = expiry
+            self.did_expire = expiry < now
+            return True #successfully performed authorisation
+
+        return False #authorisation was unsuccessful
 
     def get_access_token(self):
         token = self.access_token
-        expires = self.access_token_expires
         now = datetime.datetime.now()
-        if expires <= now:
+        if self.token_expiry < now:
             self.authorise()
             return self.get_access_token()
         elif token == None:
-            self.perform_auth()
+            self.authorise()
             return self.get_access_token() 
         return token
 
 if __name__ == '__main__':
     sa = SpotifyAuthorisation()
-    sa.authorise()
-
-
-# Make sure to fill in your spotify client_secret information
-# spotify_token = "BQC2rXOFN1nhwfWEmf9PTN_gKYzdDNSeIBmq1quLnCUWk3WpxC3HSiNjaV2FsmTCRPq1nM062jE8aEXE44RXNtXAVeJFw2xcibA1cLdobG674zg9DOYqjx5_qw26EpjKPt--8-74UYkxWApb95vTpjs8k-3GDnWzlzvmqfn32zsSEczaGPVamIpmIen0E4mzMmHHPdAHeklIJr0Xz-8sLZWH2SJyvorAOSzgEhNV6bhg0oI9dj3_6jFqIhf3mnylT6H9FRmg6ygecoVT"
-# spotify_user_id = "dotti22"
-
-#TO-DO need to fix token 
-
-# Necessary Scopes
-# - playlist-modify-public
-# - playlist-modify-private
-# - user-library-read
-
-#Can generate OAuth Token from here: https://developer.spotify.com/console/post-playlists/
+    token = sa.get_access_token()
+    print(token)
